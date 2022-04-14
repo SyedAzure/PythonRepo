395c285fc7e63e7621de24fdc209261518787bf4
ahSyed <ahsyed84@hotmail.com>
2022-04-14 01:12:54 -0400

    additional line added

--- demo1.py
+++ demo1.py
@@ -1,2 +1,4 @@
 
-Print("Hello Testing Python File from GIT Repo")
+print("Hello Testing Python File from GIT Repo")
+
+print("wrote code in pycharm and commit testing to save into GIT other way around")
