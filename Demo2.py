3eb612663bc5f1e983826e7f812188282e198e50
ahSyed <ahsyed84@hotmail.com>
2022-04-15 00:07:26 -0400

    New Variable Code added

--- Demo2.py
+++ Demo2.py
@@ -1,3 +1,19 @@
+from math import *
 
-
-print("Test New File")
+my_num = 10
+print(50)
+print(50 + 10) # Add 2 Numbers
+print(30 - 10) # Sub 2 Numbers
+print(10 * 12) # Mul 2 Numbers
+print(100 / 10) # Div 2 Numbers
+print(3 * 5 + 2) # Calculate with Sequence
+print(3 * (5 + 2)) # First Add and then multiple
+print(10 % 3) # Modulates operator which will give us the remainder value
+print(str(my_num) + " Convert Number to String") # Convert Number into String and then Print
+print(abs(my_num)) # Get absolute value of the Num
+print(pow(4, 2)) # Power Function is used for taking specific numbers and do the operation
+print(max(6, 3, 8, 2), min(6, 3, 8, 2)) # Get Max & Min value from the given Numbers
+print(round(3.2), round(3.5), round(4.7)) # Round the Value and give output
+print(floor(3.3)) # Print only Number and avoid decimals for this we need to import math package
+print(ceil(3.4)) # Round the no up words any thing above 3 it will round to 4
+print(sqrt(36)) # Get the Square root of the given no
