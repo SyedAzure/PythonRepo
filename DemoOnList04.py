0db444e236a3ef8ac9659baaa4b69df32532c63c
ahSyed <ahsyed84@hotmail.com>
2022-04-15 16:15:41 -0400

    New Variable Code added

--- DemoOnList04.py
+++ DemoOnList04.py
@@ -1,2 +1,6 @@
 
-print("Demo on List")
+lst = ["Adam", "Jacob", "Mike", 25, False]  # List can store any data type and any no of element
+    #    0        1       2      3    4      Stores using index
+print(lst[2]) # Print Which ever index value we give.
+print(lst[-1])  # Print reverse
+
