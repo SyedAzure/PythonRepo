1741512ce1a7c4af2d7e666ab6b65807f27fc093
ahSyed <ahsyed84@hotmail.com>
2022-04-14 21:07:25 -0400

    New Variable Code added

--- demo1.py
+++ demo1.py
@@ -1,15 +1,10 @@
 
 Abc = "Test My Local Repo"
+State = "United States"
 
 print("Hello Testing Python File from GIT Repo")
-print("Hello Broooooooo!")
-
-
-print(Abc)
-
-print(Abc+' This is a test')
-print(Abc+' This is a test')
-print(Abc+' This is a test')
-print(Abc+' This is a test')
-print(Abc+' This is a test')
-print(Abc+' This is a test')
+print("Testing \nApplication") # Print in 2 different lines
+print(len(Abc)) # Get the length of an string
+print(State[0]) # Get the particular charecter from the string
+print(State.index("S")) # Tell us the Exact location of that Variable available in the string
+print(State.replace("States","Kingdom")) # Replace the States with Kingdom String
