57b3ec07811336838bf3ff7f1b10c66379224b3b
ahSyed <ahsyed84@hotmail.com>
2022-04-16 12:56:55 -0400

    New Variable Code added

--- DemoOnList04.py
+++ DemoOnList04.py
@@ -8,19 +8,25 @@ print(lst[-1])  # Print reverse
 print(lst[2:])  # If you want to print a value after certain index then it will print like this
 print(lst[2:4])  # If you want to print particular index
 
-lucky_numbers = [2, 4, 8, 16, 32, 64, 128]
-frnds = ["Alpha", "Beta", "Ghama", "Mike", "Johnson", "Zaid", "Zaid"]
-frnds.append("Zakir") # Add the new name into the frnds list.
-frnds.extend(lucky_numbers) # Add Lucky_numbers list ro frnds list and print
-print(frnds) # print Both the list together.
-print(frnds.count("Zaid")) # Count the same element and print the No
-frnds.insert(1, "Alphabets") # Insert new word in the index 1 and print
-print(frnds)
-frnds.remove("Alpha") # Remove the particular word from the given list
-print(frnds)
-frnds.pop() # Pop delete the last element from the list
-print(frnds)
-print(frnds.index("Zaid")) # Search for particular element in the list and print the index no
-frnds.clear() # Clear the List
-print(frnds)
+lst1_num = [2, 4, 8, 16, 32, 64, 128]
+frnds_list = ["Don", "Alpha", "Beta", "Ghama", "Mike", "Johnson", "Zaid", "Zaid"]
+frnds_list.sort() # Sort the Friend list in alphabetics
+print(frnds_list)
+frnds_list.reverse() # Reverse the list order and print
+print(frnds_list)
+frinds2 = frnds_list.copy() # Copy the frnds_list into frinds2 list. copt from 1 list to other
+print(frinds2)
+frnds_list.append("Zakir") # Add the new name into the frnds list.
+frnds_list.extend(lst1_num) # Add Lucky_numbers list ro frnds list and print
+print(frnds_list) # print Both the list together.
+print(frnds_list.count("Zaid")) # Count the same element and print the No
+frnds_list.insert(1, "Alphabets") # Insert new word in the index 1 and print
+print(frnds_list)
+frnds_list.remove("Alpha") # Remove the particular word from the given list
+print(frnds_list)
+frnds_list.pop() # Pop delete the last element from the list
+print(frnds_list)
+print(frnds_list.index("Zaid")) # Search for particular element in the list and print the index no
+frnds_list.clear() # Clear the List
+print(frnds_list)
 
