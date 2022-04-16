9dd7c63141acebc5a989e890eddc2d48882545dc
ahSyed <ahsyed84@hotmail.com>
2022-04-16 13:34:04 -0400

    New Variable Code added

--- DemoOntuples.py
+++ DemoOntuples.py
@@ -1,5 +1,7 @@
 
 print("tuples are like list. tuples values cannot cange and once you create and cannot change its structure in list u have a option to change")
-
+print("tuples values is declared using () and list by using []. Tuples are used when values dont need to change")
 cordinates = (10, 20) # Tuples store values in index and we can print based on index value
 print(cordinates[1])
+cordinates2 = [(5, 10), (15, 20), (25, 30)] # Multiple tuples values declared
+print(cordinates2[1])
