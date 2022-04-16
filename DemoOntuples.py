a7ac5b1f66ba62ccb23664c9a5751fa7eb007713
ahSyed <ahsyed84@hotmail.com>
2022-04-16 13:06:12 -0400

    New Variable Code added

--- DemoOntuples.py
+++ DemoOntuples.py
@@ -1,2 +1,5 @@
 
-print("tuples sample")
+print("tuples are like list. tuples values cannot cange and once you create and cannot change its structure in list u have a option to change")
+
+cordinates = (10, 20) # Tuples store values in index and we can print based on index value
+print(cordinates[1])
